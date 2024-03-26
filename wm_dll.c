770634b3c49d4692c89c71ce27df77dc786537c1
alp <76008113+interdict0r@users.noreply.github.com>
2024-03-27 02:31:39 +0300

    bug fix
    fixed window overlapping

--- wm_dll.c
+++ wm_dll.c
@@ -3,9 +3,28 @@
 
 __declspec(dllexport) LRESULT CALLBACK ShellProc(int code, WPARAM wparam, LPARAM lparam) {
 
+    static int windowCount = 0;
+
     if(code == HSHELL_WINDOWCREATED || code == HSHELL_WINDOWDESTROYED) {
-        // null parameter for whole parent window
-        TileWindows(NULL, MDITILE_VERTICAL, NULL, NULL, NULL);
+
+        HWND desktop = GetDesktopWindow();
+        HWND child = GetWindow(desktop, GW_CHILD);
+        int newWindowCount = 0;
+
+        while(child) {
+
+            newWindowCount += !IsIconic(child);
+            child = GetWindow(child, GW_HWNDNEXT);
+        }
+
+        if(newWindowCount > windowCount) {
+            if(newWindowCount > 1) {
+                // null parameters for whole parent window
+                TileWindows(NULL, MDITILE_VERTICAL, NULL, NULL, NULL);
+            }
+
+            windowCount = newWindowCount;
+        }
     }
 
     return CallNextHookEx(NULL, code, wparam, lparam);
